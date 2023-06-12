bc05d839924ef758a7a7b2437643b3ca494e3679
Balthazar Rouberol <br@imap.cc>
2023-06-12 11:25:29 +0200

    Track darkvision

--- 5esheets/app.py
+++ 5esheets/app.py
@@ -35,7 +35,7 @@ def create_table():
 def is_field_from_checkbox(field_name):
     return (
         field_name.endswith("-prof")
-        or field_name == "inspiration"
+        or field_name in ("inspiration", "darkvision")
         or field_name.startswith(("deathfail", "deathsuccess"))
     )
 
