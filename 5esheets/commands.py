9094564cb1079da56426bf11a1e80072be8e4ea6
Balthazar Rouberol <br@imap.cc>
2023-06-22 09:19:28 +0200

    Replace Peewee by SQLAlchemy, to support asynchronous queries in the future

--- 5esheets/commands.py
+++ 5esheets/commands.py
@@ -1,9 +1,10 @@
 import json
+from contextlib import closing
 
 import click
 from flask.cli import AppGroup
 
-from .db import db, db_dir
+from .db import create_session, db_dir
 from .models import Character, Party, Player
 
 db_commands = AppGroup("db")
@@ -14,18 +15,21 @@ def populate_db():
     with open(db_dir / "fixtures" / "dev.json") as dev_fixtures_fd:
         dev_fixtures = json.load(dev_fixtures_fd)
 
-    for player_attrs in dev_fixtures["players"]:
-        Player.replace(**player_attrs).execute()
-        player = Player.get_by_id(player_attrs["id"])
-        click.echo(f"Player {player} saved")
+    session = create_session()
+    with closing(session):
+        for player_attrs in dev_fixtures["players"]:
+            player = Player(**player_attrs)
+            session.merge(player)
+            click.echo(f"Player {player} saved")
 
-    for party_attrs in dev_fixtures["parties"]:
-        Party.replace(**party_attrs).execute()
-        party = Party.get_by_id(party_attrs["id"])
-        click.echo(f"Party {party} saved")
+        for party_attrs in dev_fixtures["parties"]:
+            party = Party(**party_attrs)
+            session.merge(party)
+            click.echo(f"Party {party} saved")
 
-    for character_attrs in dev_fixtures["characters"]:
-        json_data = json.dumps(character_attrs.pop("json_data"))
-        Character.replace(json_data=json_data, **character_attrs).execute()
-        character = Character.get_by_id(character_attrs["id"])
-        click.echo(f"Character {character} saved")
+        for character_attrs in dev_fixtures["characters"]:
+            json_data = json.dumps(character_attrs.pop("json_data"))
+            character = Character(json_data=json_data, **character_attrs)
+            session.merge(character)
+            click.echo(f"Character {character} saved")
+        session.commit()
