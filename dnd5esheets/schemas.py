6e14293655d775242d58d713c534eb9054075a8d
Balthazar Rouberol <br@imap.cc>
2023-06-22 14:44:29 +0200

    Embed field descriptions that are rendered in the API doc

--- dnd5esheets/schemas.py
+++ dnd5esheets/schemas.py
@@ -11,38 +11,42 @@ class BaseORMSchema(BaseSchema):
 
 
 class PlayerSchema(BaseORMSchema):
-    id: int = Field(gt=0)
-    name: constr(max_length=255)
+    id: int = Field(ge=1, title="The player primary key in database")
+    name: str = Field(max_length=255, title="The player name")
 
 
 class PartySchema(BaseORMSchema):
-    id: int = Field(gt=0)
-    name: constr(max_length=255)
+    id: int = Field(ge=1, title="The party primary key in database")
+    name: str = Field(max_length=255, title="The party name")
 
 
 class CharacterSchema(BaseORMSchema):
-    id: int = Field(gt=0)
-    name: constr(max_length=255)
-    slug: constr(max_length=255)
-    class_: constr(max_length=80)
-    level: int = Field(gt=0)
-    data: dict
-    player: PlayerSchema
-    party: PartySchema
+    id: int = Field(ge=1, title="The character primary key in database")
+    name: str = Field(max_length=255, title="The character name")
+    slug: str = Field(
+        max_length=255, title="The character slug, used to identify it in the API"
+    )
+    class_: str = Field(max_length=80, title="The character class")
+    level: int = Field(ge=1, title="The character level")
+    data: dict = Field(description="The embdedded character sheet JSON data")
+    player: PlayerSchema = Field(title="The embedded character's player schema")
+    party: PartySchema = Field(title="The embedded character's party schema")
 
 
 class ListCharacterSchema(BaseORMSchema):
-    id: int = Field(gt=0)
-    name: constr(max_length=255)
-    slug: constr(max_length=255)
-    class_: constr(max_length=80)
-    level: int = Field(gt=0)
-    player: PlayerSchema
-    party: PartySchema
+    id: int = Field(ge=1, title="The character primary key in database")
+    name: str = Field(max_length=255, title="The character name")
+    slug: str = Field(
+        max_length=255, title="The character slug, used to identify it in the API"
+    )
+    class_: str = Field(max_length=80, title="The character class")
+    level: int = Field(ge=1, le=20, title="The character level")
+    player: PlayerSchema = Field(title="The embedded character's player schema")
+    party: PartySchema = Field(title="The embedded character's party schema")
 
 
 class UpdateCharacterSchema(BaseSchema):
-    name: constr(max_length=255) | None
-    class_: constr(max_length=80) | None
-    level: int | None = Field(gt=0)
-    data: dict | None
+    name: str | None = Field(title="A new character name (Optional)")
+    class_: str | None = Field(max_length=80, title="A new character class (Optional)")
+    level: int | None = Field(ge=1, title="A new character level (Optional)")
+    data: dict | None = Field(title="Updates to the character sheet fields (Optional)")
