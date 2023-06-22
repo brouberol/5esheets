605ab122e494772320a58ceceb6d9bde05a321e1
Balthazar Rouberol <br@imap.cc>
2023-06-22 12:56:11 +0200

    Add docstrings to the API handlers

--- dnd5esheets/app.py
+++ dnd5esheets/app.py
@@ -15,6 +15,11 @@ app = FastAPI()
 def list_characters(
     session: Session = Depends(create_scoped_session),
 ) -> list[ListCharacterSchema]:
+    """List all characters.
+
+    The returned payload will not include the character sheet details.
+
+    """
     return CharacterRepository.list_all(session)
 
 
@@ -22,6 +27,7 @@ def list_characters(
 def display_character(
     slug: str, session: Session = Depends(create_scoped_session)
 ) -> CharacterSchema:
+    """Display all details of a given character."""
     character = CharacterRepository.get_by_slug(session, slug=slug)
     if character is None:
         raise HTTPException(status_code=404, detail="Character not found")
@@ -34,6 +40,18 @@ def update_character(
     character_data: UpdateCharacterSchema,
     session: Session = Depends(create_scoped_session),
 ) -> dict:
+    """Update a character details.
+
+    Examples of JSON body paylods:
+
+    - `{"level": 5 }`
+    - `{"name": "Toto"}`
+    - `{"class_": "Guerrier", "data": {"background": "Folk Hero"}}`
+
+    In the last example, we update both a direct character attribute
+    as well as an attribute nested in the character JSON data.
+
+    """
     character = CharacterRepository.update_character(session, slug, character_data)
     if character is None:
         raise HTTPException(status_code=404, detail="Character not found")
