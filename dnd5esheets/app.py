91f4811dc9e6788306c4518081ac0cd6cc89e044
Balthazar Rouberol <br@imap.cc>
2023-06-23 13:14:51 +0200

    Serve the svelte built files via the FastAPI app

--- dnd5esheets/app.py
+++ dnd5esheets/app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
 
-from .repositories import ModelNotFound
 from .api import api
-
+from .repositories import ModelNotFound
 
 app = FastAPI()
 app.include_router(api)
@@ -14,3 +14,6 @@ app.include_router(api)
 def raise_404_exception_on_model_not_found(_: Request, exc: Exception):
     """Return a 404 response when handling a ModelNotFound exception"""
     return JSONResponse(content={"detail": str(exc)}, status_code=404)
+
+
+app.mount("", StaticFiles(directory="client/dist/", html=True), name="static")
