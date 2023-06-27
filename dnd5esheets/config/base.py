7cd72bb16a8e230140bf3ed776cdbb275482dc97
Balthazar Rouberol <br@imap.cc>
2023-06-27 22:44:23 +0200

    Define a test configuration

--- dnd5esheets/config/base.py
+++ dnd5esheets/config/base.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 from pydantic import BaseSettings
+
+db_dir = Path(__file__).parent.parent / "db"
+db_file = db_dir / "5esheets.db"
 
 
 class CommonSettings(BaseSettings):
     JWT_ENCODING_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 12 * 60  # in mins
     MULTITENANT_ENABLED: bool = False
+    DB_URI: str = f"sqlite:///{db_file}"
+    DB_ASYNC_URI: str = f"sqlite+aiosqlite:///{db_file}"
--- dnd5esheets/config/prod.py
+++ dnd5esheets/config/prod.py
@@ -3,6 +3,8 @@ from .base import CommonSettings
 
 class ProdSettings(CommonSettings):
     SECRET_KEY: str
+    DB_URI: str
+    DB_ASYNC_URI: str
     MULTITENANT_ENABLED: bool = True
 
     class Config:
