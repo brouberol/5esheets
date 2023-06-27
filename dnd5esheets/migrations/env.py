2707992af8f2365f189e26c272c70be6efe11cc0
Balthazar Rouberol <br@imap.cc>
2023-06-27 15:12:24 +0200

    Configure the alembic db engine to generate SQLite-compatible migrations

--- dnd5esheets/migrations/env.py
+++ dnd5esheets/migrations/env.py
@@ -1,9 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 from dnd5esheets.models import BaseModel
 
@@ -46,6 +44,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        render_as_batch=True,
     )
 
     with context.begin_transaction():
@@ -66,7 +65,11 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=True,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
