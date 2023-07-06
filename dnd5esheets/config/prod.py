772925fff418efade79376f8f53ab25cfadcc7e0
Balthazar Rouberol <br@imap.cc>
2023-07-06 16:23:28 +0200

    Setup CORS to allow API calls to come from the dev frontend server (#101)
    * Setup CORS to allow API calls to come from the dev frontend server
    
    * Configure the base API host according to the NODE_ENV environment variable
    
    * Don't configure CORS in prod, as the static will be hitting the same domain
    
    * Define the root of the 5esheet API client as a make variable

--- dnd5esheets/config/prod.py
+++ dnd5esheets/config/prod.py
@@ -7,6 +7,7 @@ class ProdSettings(CommonSettings):
     DB_URI: str
     DB_ASYNC_URI: str
     MULTITENANT_ENABLED: bool = True
+    FRONTEND_CORS_ORIGIN: str | None = None
 
     class Config:
         env_file = ".env"
