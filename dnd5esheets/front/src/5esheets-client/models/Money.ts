1c2183137b0aa37657133a8fef48eca5e5a147e0
Balthazar Rouberol <br@imap.cc>
2023-07-09 17:16:54 +0200

    Add missing model file

---
+++ dnd5esheets/front/src/5esheets-client/models/Money.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type Money = {
+    copper: number;
+    silver: number;
+    electrum: number;
+    gold: number;
+    platinum: number;
+};
+
