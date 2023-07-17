1a387e22a197a3feb4547c0ba1731d05784f3bae
Balthazar Rouberol <br@imap.cc>
2023-07-17 15:56:17 +0200

    List characters by hitting the /api/character/ API (#121)
    * List characters by hitting the /api/character/ API
    
    * Run prettier

--- dnd5esheets/front/src/components/CharacterList.tsx
+++ dnd5esheets/front/src/components/CharacterList.tsx
@@ -2,17 +2,19 @@ import { A } from "@solidjs/router";
 import { createSignal } from "solid-js";
 import { css } from "solid-styled";
 
-import { CharacterSchema } from "~/5esheet-client";
+import { ListCharacterSchema } from "~/5esheet-client";
 
 export default function CharacterList({
   characters,
 }: {
-  characters: CharacterSchema[];
+  characters: ListCharacterSchema[];
 }) {
   return (
     <ul>
       {Object.values(characters).map(({ name, slug }) => (
-        <A href={"character/" + slug}>{name}</A>
+        <li>
+          <A href={"character/" + slug}>{name}</A>
+        </li>
       ))}
     </ul>
   );
--- dnd5esheets/front/src/routes/index.tsx
+++ dnd5esheets/front/src/routes/index.tsx
@@ -1,14 +1,18 @@
 import { Title } from "solid-start";
 import CharacterList from "~/components/CharacterList";
-import useStore from "~/store";
+import { CharacterService } from "~/5esheets-client";
+import { createResource } from "solid-js";
+
+const listCharacters = async () => {
+  return await CharacterService.listCharacters();
+};
 
 export default function Home() {
-  const [characters, { update }] = useStore();
-
+  const [characters] = createResource(listCharacters);
   return (
     <main>
       <Title>D&D 5e sheets</Title>
-      <CharacterList characters={characters} />
+      {characters() && <CharacterList characters={characters()} />}
     </main>
   );
 }
