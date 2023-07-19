a4cba6f3985c8ad49459727fd85c5c0457c5df79
Balthazar Rouberol <br@imap.cc>
2023-07-19 19:20:31 +0200

    Add missing file

---
+++ dnd5esheets/front/src/5esheets-client/models/CreateCharacterSchema.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * All details associated with a character
+ */
+export type CreateCharacterSchema = {
+    name: string;
+    party_id: number;
+};
+
