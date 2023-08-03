02f073abebe7e82ccec1590f5768794d5903a096
Balthazar Rouberol <br@imap.cc>
2023-08-03 08:20:18 +0200

    Rely on starlette status constants

--- dnd5esheets/app.py
+++ dnd5esheets/app.py
@@ -39,13 +39,17 @@ dist_dir = Path(__file__).parent / "front" / "dist"
 @app.exception_handler(ModelNotFound)
 def raise_404_exception_on_model_not_found(_: Request, exc: Exception):
     """Return a 404 response when handling a ModelNotFound exception"""
-    return JSONResponse(content={"detail": str(exc)}, status_code=404)
+    return JSONResponse(
+        content={"detail": str(exc)}, status_code=status.HTTP_404_NOT_FOUND
+    )
 
 
 @app.exception_handler(DuplicateModel)
 def raise_400_exception_on_duplicate_model(_: Request, exc: Exception):
     """Return a 400 response when handling a DuplicateModel exception"""
-    return JSONResponse(content={"detail": str(exc)}, status_code=400)
+    return JSONResponse(
+        content={"detail": str(exc)}, status_code=status.HTTP_400_BAD_REQUEST
+    )
 
 
 @app.exception_handler(CacheHit)
