99ca1331095c634443ef787563a63198906739e0
Balthazar Rouberol <br@imap.cc>
2023-08-04 10:55:54 +0200

    Add APIs to add/remove and item to/from the character's equipment

--- dnd5esheets/repositories/item.py
+++ dnd5esheets/repositories/item.py
@@ -1,17 +1,6 @@
-from typing import cast
-
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from dnd5esheets.models import Item
 from dnd5esheets.repositories import BaseRepository
 
 
 class ItemRepository(BaseRepository):
     model = Item
-
-    @classmethod
-    async def get_by_id(cls, session: AsyncSession, id: int) -> Item:
-        query = select(Item).filter(Item.id == id)
-        result = await session.execute(query)
-        return cast(Item, cls.one_or_raise_model_not_found(result))
