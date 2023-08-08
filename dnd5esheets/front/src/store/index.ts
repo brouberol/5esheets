0938ebb34b67a33c86322cc85c0e3696b38b737a
Etienne <etn@etnbrd.com>
2023-08-08 22:55:46 +0200

    wire effects on inserting character into store

--- dnd5esheets/front/src/store/index.ts
+++ dnd5esheets/front/src/store/index.ts
@@ -1,16 +1,69 @@
+import { createEffect, createMemo } from 'solid-js'
 import { SetStoreFunction, createStore, produce } from 'solid-js/store'
 
 import {
+  Abilities,
   CharacterSchema,
   CharacterSheet,
   OpenAPI,
   Proficiency,
 } from '~/5esheets-client'
 import { CharacterService } from '~/5esheets-client'
+import { applyEffects, computeEffect } from '~/effects'
 
 if (process.env.NODE_ENV === 'development') {
   OpenAPI.BASE = 'http://localhost:8000'
 }
+
+type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${'' extends P ? '' : '.'}${P}`
+    : never
+  : never
+
+type Split<S extends string, D extends string> = string extends S
+  ? string[]
+  : S extends ''
+  ? []
+  : S extends `${infer T}${D}${infer U}`
+  ? [T, ...Split<U, D>]
+  : [S]
+
+type Prev = [
+  never,
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  ...0[],
+]
+
+// Types all the possible path to access leaves of an object.
+type Leaves<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? { [K in keyof T]-?: Join<K, Leaves<T[K], Prev[D]>> }[keyof T]
+  : ''
+
+// Tentative of typing the possible target for an effect, based on the shape of Character.
+type Target = Leaves<Character>
 
 export const cycleProficiency = (proficiency: number) =>
   ((proficiency + 1) % 3) as Proficiency
@@ -100,6 +153,8 @@ class CharacterStore {
         c[resourceState] = 'ready'
       })
     )
+
+    this.wireEffects(this.characters[slug] as ResolvedCharacter)
   }
 
   private updateCharacter =
@@ -114,6 +169,140 @@ class CharacterStore {
         })
       )
     }
+
+  private wireEffects = (character: ResolvedCharacter) => {
+    const abilityKeys = Object.keys(character.data.abilities) as Array<
+      keyof Abilities
+    >
+
+    const baseEffects = [
+      // proficiency bonus effect
+      {
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `data.proficiency_bonus := Math.ceil(1 + level / 4)`,
+          character
+        ),
+      },
+
+      // ability modifiers effects
+      ...abilityKeys.map((ability) => ({
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `data.abilities.${ability}.modifier := Math.floor((data.abilities.${ability}.score - 10) / 2)`,
+          character
+        ),
+      })),
+
+      // ability saves effects
+      ...abilityKeys.map((ability) => ({
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `data.abilities.${ability}.save := data.abilities.${ability}.modifier + data.abilities.${ability}.proficiency * data.proficiency_bonus`,
+          character
+        ),
+      })),
+
+      // skills effects
+      ...[
+        ['acrobatics', 'dexterity'],
+        ['animal_handling', 'wisdom'],
+        ['arcana', 'intelligence'],
+        ['athletics', 'strength'],
+        ['deception', 'dexterity'],
+        ['history', 'intelligence'],
+        ['insight', 'wisdom'],
+        ['intimidation', 'charisma'],
+        ['investigation', 'intelligence'],
+        ['medicine', 'wisdom'],
+        ['nature', 'intelligence'],
+        ['perception', 'wisdom'],
+        ['performance', 'charisma'],
+        ['persuasion', 'charisma'],
+        ['religion', 'intelligence'],
+        ['sleight_of_hand', 'dexterity'],
+        ['stealth', 'dexterity'],
+        ['survival', 'wisdom'],
+      ].map(([skill, ability]) => ({
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `data.skills.${skill}.modifier := data.abilities.${ability}.modifier + data.skills.${skill}.proficiency * data.proficiency_bonus`,
+          character
+        ),
+      })),
+
+      // initiative effect
+      {
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `data.initiative := data.abilities.dexterity.modifier`,
+          character
+        ),
+      },
+
+      // passive perception effect
+      {
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `data.passive_perception := 10 + data.skills.perception.modifier`,
+          character
+        ),
+      },
+
+      // spell DC effect
+      {
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `spell_dc := 8 + data.scores[data.spellcasting_ability] + data.proficiency_bonus`,
+          character
+        ),
+      },
+
+      // spell attack bonus effect
+      {
+        name: 'base',
+        priority: 10,
+        ...computeEffect(
+          `spell_attack_bonus := data.scores[data.spellcasting_ability] + data.proficiency_bonus`,
+          character
+        ),
+      },
+    ]
+
+    const targets = baseEffects.reduce(
+      (set, effect) => set.add(effect.target),
+      new Set<Target>()
+    )
+
+    // Create reactive effects to recompute target derived attributes when characteristics change
+    for (const derived of targets) {
+      const memo = createMemo(() => {
+        const sortedEffects = [
+          ...baseEffects.filter(({ target }) => target === derived),
+          // TODO here goes the equipement effects
+        ]
+          .sort((a, b) => b.priority - a.priority)
+          .map((effect) => ({ ...effect, value: createMemo(effect.value) }))
+
+        return applyEffects<Target>(sortedEffects)
+      })
+
+      const path = derived.split('.') as Split<typeof derived, '.'>
+
+      // TODO memo() also returns the effect history, but we need to store it somewhere else in the character,
+      // to allow referencing derived attribute directly, rather than with .value at the end, e.g. data.scores.wisdom_mod.value
+      createEffect(() => {
+        this.setCharacters(character.slug, ...path, memo().value)
+      })
+    }
+  }
 }
 
 function throwError(message: string) {
