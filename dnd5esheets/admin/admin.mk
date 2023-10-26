a2f6e6a59a2eca24730ed597e627edf04d331bd0
Balthazar Rouberol <br@imap.cc>
2023-10-26 16:53:11 +0200

    Use the lightbulb pygments theme to display json now that the pre background is dark

--- dnd5esheets/admin/admin.mk
+++ dnd5esheets/admin/admin.mk
@@ -1,6 +1,6 @@
 .PHONY=admin
 
 $(app-root)/admin/statics/pygments.css:
-	@poetry run pygmentize -S friendly -f html -a .highlight > $(app-root)/admin/statics/pygments.css
+	@poetry run pygmentize -S lightbulb -f html -a .highlight > $(app-root)/admin/statics/pygments.css
 
 admin-statics: $(app-root)/admin/statics/pygments.css
